1bcb58f93d26134bdb951f5b9f03bd715c2cd4b3
shreeharini132025 <shreeharini132025@gmail.com>
2026-06-11 12:15:49 +0530

    backend server updated

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const corsOptions = {
 
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
